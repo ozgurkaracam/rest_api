684ec6b070b21954e68c08beeb9b65b6d0a71657
ozgurkaracam <ozgurkaracam55@gmail.com>
2021-01-14 17:26:07 +0300

    tailwind,vue3 setups

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <p class="text-lg text-center">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+    </p>
+</template>
+<script>
+    export default {
+        name: 'App',
+        mounted() {
+            console.log('ok')
+        }
+    }
+</script>
